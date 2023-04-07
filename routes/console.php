bbf8bc26932d32d50118efdb9cea3d14f3db3ceb
hugoresende27 <hugoresende27@gmail.com>
2023-04-07 23:37:06 +0100

    Process::run / fake

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Process;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,45 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('list-assert', function () {
+    Process::fake(['ls -la' => 'fake list of dir']);
+
+    Process::run('ls -la');
+    Process::assertRan('ls -la');
+});
+
+Artisan::command('fake', function () {
+    Process::fake(['git log' => 'fake git log']);
+
+    $this->info(Process::run('git log')->output());
+    $this->info(Process::run('npm run build')->output());
+});
+
+Artisan::command('npm-build-2', function () {
+    $this->info(Process::run('npm run build')->output());
+});
+
+Artisan::command('npm-build-1', function () {
+    Process::run('npm run build', function ($type, $output) {
+        $this->info($output);
+    });
+});
+
+Artisan::command('gitlog', function () {
+    $this->info(Process::run('git log')->output());
+});
+
+
+Artisan::command('npm-build-3', function () {
+
+    $process = Process::start('npm run build');
+    //do something while wait
+    while ($process->running()){
+       $this->info('Working....');
+       sleep(1);
+    }
+    //wait to give the info all at once
+    $process->wait();
+    $this->info($process->output());
+});
