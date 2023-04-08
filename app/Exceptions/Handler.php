a27a42dbc177fadab2c7233c19f796174ec943b2
hugoresende27 <hugoresende27@gmail.com>
2023-04-08 19:30:55 +0100

    Error Handler tests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+
 
 class Handler extends ExceptionHandler
 {
@@ -45,4 +49,49 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Exception|Throwable $exception)
+    {
+        if ($exception instanceof HttpException) {
+            echo "<pre>";
+            var_dump($exception);
+            die();
+            return response()->json([
+                'error' => $exception->getMessage()
+            ], $exception->getStatusCode());
+        }
+
+        return parent::render($request, $exception);
+    }
+
+
+
+//    public function render($request, Throwable $exception)
+//    {
+//        if ($exception instanceof HttpException) {
+//            $statusCode = $exception->getStatusCode();
+//            $error = $exception->getMessage();
+//
+//            if (app()->environment() == 'local') {
+//                $errorInfo = [
+//                    'file' => $exception->getFile(),
+//                    'line' => $exception->getLine(),
+//                    'trace' => $exception->getTrace()
+//                ];
+//
+//                return response()->json([
+//                    'error' => $error,
+//                    'error_info' => $errorInfo
+//                ], $statusCode);
+//            }
+//
+//            return response()->json([
+//                'error' => $error,
+//            ], $statusCode);
+//        }
+//
+//        return parent::render($request, $exception);
+//    }
+
+
 }
